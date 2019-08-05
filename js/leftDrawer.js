08b56d19d368d813835f96f01c477a35b06c8297
Shelly Lire <iam7seven7@gmail.com>
2019-08-05 23:56:16 +0800

    Fix drawer class not found error

--- js/leftDrawer.js
+++ js/leftDrawer.js
@@ -7,17 +7,19 @@
         $("#nav_bar").appendTo('#leftDrawer');
         $("#nav_bar nav").addClass('drawer-nav');
         $("#reference_link_container").appendTo(".drawer-nav");
-      }else{
+      } else {
         $("#nav_bar").prependTo('#mainContent');
         $("#nav_bar nav").removeClass('drawer-nav');
         $("#reference_link_container").insertAfter("#mobileMenuBtn");
       }
     });
-    $('.drawer').drawer();
-    $('.drawer-nav a').on("click",function(){
-      $('body').removeClass('drawer-open');
-      $('body').addClass('drawer-close');
-    }
-  )
+
+    $(window).on("load resize",function(){
+      $('.drawer').drawer();
+      $('.drawer-nav a').on("click",function(){
+        $('body').removeClass('drawer-open');
+        $('body').addClass('drawer-close');
+      });
+    });
   });
 })();
