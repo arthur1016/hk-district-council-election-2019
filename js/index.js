4c9cc861b33559b2856e46e08879369228736660
Arthur Chan <excaelibur1016@gmail.com>
2019-11-29 15:32:48 +0800

    UI Update After Election, Contact update for A&B

--- js/index.js
+++ js/index.js
@@ -1,25 +1,25 @@
 //Functions
 function showRegions(path, toDraw) {
   $.getJSON(path, function (data) {
-	  var delibratingRegions = 0;
-      var uncontestedRegions = 0;
+	  var demPartyRegions = 0;
+      var demIndependentRegions = 0;
       $.each(data, function (index, value) {
-          if (value.contestStatus == "UNCON") {
-              if(toDraw){L.polygon([value.points], { className: value.code, color: "#FF0000" }).addTo(map);}
-              uncontestedRegions = uncontestedRegions + 1;
-          } else if (value.contestStatus == "DELIB") {
-			  if(toDraw){L.polygon([value.points], { className: value.code, color: "#FF9A26" }).addTo(map);}
-              delibratingRegions = delibratingRegions + 1;
-		  } else { //value.contestStatus == "CON"
+          if (value.contestStatus == "PARTY") {
+              if(toDraw){L.polygon([value.points], { className: value.code, color: "#FFFF00" }).addTo(map);}
+              demPartyRegions = demPartyRegions + 1;
+          } else if (value.contestStatus == "INDPNT") {
+			  if(toDraw){L.polygon([value.points], { className: value.code, color: "#FFCC00" }).addTo(map);}
+              demIndependentRegions = demIndependentRegions + 1;
+		  } else { //value.contestStatus == "ESTAB"
 			  if(toDraw){L.polygon([value.points], { className: value.code}).addTo(map);}
           }
       });
 	  
-	  var defaultText = "<p class='defaultText'><u>勿忘行動訴求</u><br/>完全撤回逃犯修例<br/>撤銷6月12日暴動定性<br/>撤銷反送中抗爭者控罪<br/>成立獨立調查委員會<br/>立即實行雙普選</p>";
+	  var defaultText = "<p class='defaultText'><u>勿忘五大訴求</u><br/>完全撤回逃犯修例<br/>撤銷6月12日暴動定性<br/>撤銷反送中抗爭者控罪<br/>成立獨立調查委員會<br/>立即實行雙普選</p>";
 	  
 	  var d = new Date();
       var datetime = d.getFullYear() + "年" + (d.getMonth() + 1) + "月" + d.getDate() + "日";// + d.getHours() + "時" + d.getMinutes() + "分";
-      $("#entry_status").html("截至" + datetime + ",在全部452個選區中,已確認<span style='color: blue;'>" + (452-uncontestedRegions-delibratingRegions).toString() + "個選區</span>有較大機會出現競爭；並有<span style='color: #FF9A26;'>" + delibratingRegions.toString() + "個選區</span>有人積極考慮參選。" + "<br/><strong>有機會由建制派自動當選的選區<span style='color: red;'>尚餘" + uncontestedRegions.toString() + "個</span>。</strong>" + "<br/><br/>"+ defaultText);
+      $("#entry_status").html("在全部452位民選區議員中,來自<span style='background-color: #FFFF00' title='民主黨、公民黨、民協、新民主同盟、工黨、街工、社民連、人民力量、熱血公民'>民主派政黨</span>共" + demPartyRegions.toString() + "位,來自<span style='background-color: #FFCC00'>民主派地區組織或獨立人士</span>共" + demIndependentRegions.toString() + "位,來自<span style='background-color: #ADD8E6'>非民主派</span>共" + (452-demPartyRegions-demIndependentRegions).toString() + "位。" + "<br/><br/>"+ defaultText);
   });
 }
 
@@ -135,45 +135,50 @@ function onRegionClick(e) {
                     region_content =
                     "地區：" + value.district + "<br/>" +
                     "選區編號：" + value.code + "<br/>" +
-                    "選區名稱：" + value.name + "<br/>" +
-                    "建制派擬參選人：" + value.estab_contestant + " (" + value.estab_organization + ")<br/>";
-                    if (!value.democrat_contestant) {
-                        region_content = region_content + "<span style='color: red;'><strong>未有非建制派候選人！</strong></span><br/>"
+                    "選區名稱：" + value.name + "<br/>";
+					
+                    if (value.contestStatus == "ESTAB") {
+                        if(!value.democrat_contestant){
+							region_content = region_content + "<span style='color: red;'><strong>未有民主派社區幹事！</strong></span><br/>"
+						} else {
+							region_content = region_content + "民主派社區幹事：" + value.democrat_contestant + " (" + value.democrat_organization + ")<br/>";
+						}
+                        // style the detail Info for estab regions
+                        if(document.getElementById("infoBoard").classList.contains("demParty")) {
+                          document.getElementById("infoBoard").classList.remove("demParty");
+                        }
+						if(document.getElementById("infoBoard").classList.contains("demIndependent")) {
+                          document.getElementById("infoBoard").classList.remove("demIndependent");
+                        }
+                        if(!(document.getElementById("infoBoard").classList.contains("estab"))){
+                          document.getElementById("infoBoard").classList.add("estab");
+                        }
+                    } else if (value.contestStatus == "PARTY"){ 
+                        region_content = region_content + "民主派區議員：" + value.democrat_contestant + " (" + value.democrat_organization + ")<br/>";
                         // style the detail Info for no contestants
-                        if(document.getElementById("infoBoard").classList.contains("existing")) {
-                          document.getElementById("infoBoard").classList.remove("existing");
+                        if(document.getElementById("infoBoard").classList.contains("estab")) {
+                          document.getElementById("infoBoard").classList.remove("estab");
                         }
-						if(document.getElementById("infoBoard").classList.contains("inDeliberation")) {
-                          document.getElementById("infoBoard").classList.remove("inDeliberation");
+						if(document.getElementById("infoBoard").classList.contains("demIndependent")) {
+                          document.getElementById("infoBoard").classList.remove("demIndependent");
                         }
-                        if(!(document.getElementById("infoBoard").classList.contains("notExisting"))){
-                          document.getElementById("infoBoard").classList.add("notExisting");
+                        if(!(document.getElementById("infoBoard").classList.contains("demParty"))){
+                          document.getElementById("infoBoard").classList.add("demParty");
                         }
-                    } else if (value.democrat_contestant == "(待定)"){ //value.contestStatus == "DELIB"
-                        region_content = region_content + "<span style='color: red;'><strong>已有人積極考慮參選！</span><br/><br/>詳情請向<a target='_blank' href='https://www.facebook.com/PowerForDemocracy/'>民主動力</a>、<a target='_blank' href='https://www.facebook.com/反自動當選運動-2510455555631639'>反自動當選運動</a>或<a target='_blank' href='https://lihkg.com/thread/1322375/page/1'>連登相關組織者</a>查詢。</strong><br/>"
-                        // style the detail Info for no contestants
-                        if(document.getElementById("infoBoard").classList.contains("existing")) {
-                          document.getElementById("infoBoard").classList.remove("existing");
-                        }
-						if(document.getElementById("infoBoard").classList.contains("notExisting")) {
-                          document.getElementById("infoBoard").classList.remove("notExisting");
-                        }
-                        if(!(document.getElementById("infoBoard").classList.contains("inDeliberation"))){
-                          document.getElementById("infoBoard").classList.add("inDeliberation");
-                        }
-                    } else {
-						region_content = region_content + "非建制派擬參選人：" + value.democrat_contestant + " (" + value.democrat_organization + ")<br/>";
+                    } else if (value.contestStatus == "INDPNT"){
+						region_content = region_content + "民主派區議員：" + value.democrat_contestant + " (" + value.democrat_organization + ")<br/>";
                         // style the detail Info contesetants
-                        if(document.getElementById("infoBoard").classList.contains("notExisting")) {
-                          document.getElementById("infoBoard").classList.remove("notExisting");
+                        if(document.getElementById("infoBoard").classList.contains("estab")) {
+                          document.getElementById("infoBoard").classList.remove("estab");
                         }
-						if(document.getElementById("infoBoard").classList.contains("inDeliberation")) {
-                          document.getElementById("infoBoard").classList.remove("inDeliberation");
+						if(document.getElementById("infoBoard").classList.contains("demParty")) {
+                          document.getElementById("infoBoard").classList.remove("demParty");
                         }
-                        if(!(document.getElementById("infoBoard").classList.contains("existing"))){
-                          document.getElementById("infoBoard").classList.add("existing");
+                        if(!(document.getElementById("infoBoard").classList.contains("demIndependent"))){
+                          document.getElementById("infoBoard").classList.add("demIndependent");
                         }
 					}
+					
                     document.getElementById("region_content").innerHTML = region_content;
                     if (!!value.democrat_contestant) {
                         organzation_content = "<br/><strong>民間組織</strong><br/>";
@@ -190,8 +195,9 @@ function onRegionClick(e) {
                           organzation_content = organzation_content + "<br/>";
                         });
                     } else {
-                      organzation_content = "<br/><strong>有意參選市民,請按自己的心意聯絡<a target='_blank' href='https://www.facebook.com/PowerForDemocracy/'>民主動力</a>、<a target='_blank' href='https://www.facebook.com/反自動當選運動-2510455555631639'>反自動當選運動</a>或<a target='_blank' href='https://lihkg.com/thread/1322375/page/1'>連登相關組織者</a>。</strong><br/>";
+                      organzation_content = "";
                     }
+					
                     document.getElementById("organzation_content").innerHTML = organzation_content;
                     popup.setLatLng(e.latlng).setContent(region_content + organzation_content).openOn(map);
                 }
